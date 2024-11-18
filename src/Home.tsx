da81e29895f2f21500e406fc5783fb5fe27d2f7d
danicolms <danicolms@icloud.com>
2024-11-18 14:59:55 +0100

    feat: activator button and split text

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,11 +1,19 @@
-import React from "react"
-import { Stage, Layer, Text } from 'react-konva';
+import React, { useState } from "react"
+import { Stage, Layer, Text, Circle } from 'react-konva';
 
 export default function Home(){
+  const [isEditMode, setIsEditMode] = useState(false)
+
     return (
         <Stage width={window.innerWidth} height={window.innerHeight}>
           <Layer>
-            <Text text="This site is under construction" fontSize={64} draggable={true}/>
+            <Text x={200} y={200} text="THIS" fontSize={100} draggable={isEditMode}/>
+            <Text x={225} y={250} text="Revelation" fontSize={64} draggable={isEditMode}/>
+            <Text x={195} y={300} text="is under" fontSize={24} draggable={isEditMode}/>
+            <Text x={200} y={400} text="CONSTRUCTION" fontSize={32} draggable={isEditMode}/>
+          </Layer>
+          <Layer>
+            <Circle onClick={() => setIsEditMode(true)}  width={200} height={100} fill="blue"/>
           </Layer>
         </Stage>
       );
