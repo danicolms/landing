36dde8178b33c8d7610d2171fd84220c00eb5f08
danicolms <danicolms@icloud.com>
2024-11-19 18:34:34 +0100

    restart: install typescript and empty Home screen

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Home from './Home';
+
+export default function App(): JSX.Element {
+  console.log("Loading app")
+  return (
+    <Home />
+  );
+}
+
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,20 +1,16 @@
-import React, { useState } from "react"
-import { Stage, Layer, Text, Circle } from 'react-konva';
+import React from 'react';
+import { Stage, Layer } from 'react-konva';
 
-export default function Home(){
-  const [isEditMode, setIsEditMode] = useState(false)
-
-    return (
-        <Stage width={window.innerWidth} height={window.innerHeight}>
-          <Layer>
-            <Text x={200} y={200} text="THIS" fontSize={100} draggable={isEditMode}/>
-            <Text x={225} y={250} text="Revelation" fontSize={64} draggable={isEditMode}/>
-            <Text x={195} y={300} text="is under" fontSize={24} draggable={isEditMode}/>
-            <Text x={200} y={400} text="CONSTRUCTION" fontSize={32} draggable={isEditMode}/>
-          </Layer>
-          <Layer>
-            <Circle onClick={() => setIsEditMode(true)}  width={200} height={100} fill="blue"/>
-          </Layer>
-        </Stage>
-      );
+function Home(): JSX.Element {
+  return (
+    <div>
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          {/* You can add shapes here later */}
+        </Layer>
+      </Stage>
+    </div>
+  );
 }
+
+export default Home;
