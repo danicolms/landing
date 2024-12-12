368ed2b4536803e00c5d4541a0bcd2b8d2da841f
danicolms <danicolms@icloud.com>
2024-12-12 15:33:14 +0100

    reset: clean slate, change of heart

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App(): JSX.Element {
+  return (
+    <div>
+      <h1>Turn your ideas into creative digital solutions </h1>
+      <p>Just the good stuff</p> <button> Let's talk</button>
+    </div>
+  );
+}
