7735c10886ce0fee0b51e5ef559122678c6e79d8
danicolms <danicolms@icloud.com>
2024-12-14 22:25:03 +0100

    chore: center content

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import "./styles.css"
+
 export default function App(): JSX.Element {
   return (
-    <div>
+    <div className="container">
       <h1>Turn your ideas into creative digital solutions </h1>
-      <p>Just the good stuff</p> <button> Let's talk</button>
+      <p>Just the good stuff</p> 
+      <button> Let's talk</button>
     </div>
   );
 }
